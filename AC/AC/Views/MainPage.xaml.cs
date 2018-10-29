8ab4823a51e61a974040d45edd1e2d8d11e1e5a9
szymon <szcz761@gmail.com>
2018-10-29 08:19:00 +0100

    nanana

--- AC/AC/Views/MainPage.xaml.cs
+++ AC/AC/Views/MainPage.xaml.cs
@@ -2,7 +2,6 @@
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Telerik.XamarinForms.Primitives;
 using Xam.Plugin.TabView;
 using System.Collections.Generic;
 using AC.ViewModels;
@@ -15,66 +14,80 @@ namespace AC.Views
     public partial class MainPage : ContentPage
     {
         ItemsViewModel viewModel = new ItemsViewModel();
-        ObservableCollection<Image> Images = new ObservableCollection<Image>();
+        List<Image> Images  = new List<Image>();
         int Column = 3;
         public MainPage()
         {
-           
-
-              
-            Images.Add(new Image { Source = "spanie" });
-            Images.Add(new Image { Source = "picie" });
-            //Images.Add(viewModel.Groups[0].Items[2].Image);
             InitializeComponent();
+            // var monkeyList = new List<Image>();
+            Images.Add(new Image { Source = "spanie", HeightRequest = 50, WidthRequest = 50 });
+            Images.Add(new Image { Source = "spanie", HeightRequest = 50, WidthRequest = 50 });
+            //Images.Add(new Image { Source = "spanie", HeightRequest = 50, WidthRequest = 50 });
+            Images.Add(new Image { Source = "spanie", HeightRequest = 50, WidthRequest = 50 });
+            //Images.Add(new Image { Source = "spanie", HeightRequest = 50, WidthRequest = 50 });
+            Images.Add(new Image { Source = "spanie", HeightRequest = 50, WidthRequest = 50 });
+            //Images.Add(new Image { Source = "spanie", HeightRequest = 50, WidthRequest = 50 });
+            // monkeyList.Add(new Image { Source = "jedzenie", HeightRequest = 10, WidthRequest = 10 });
+
+            VerticalPictures.ItemsSource = Images;
+
+
+
             viewModel.LoadItemsCommand.Execute(null);
             InitializeTabs();
             UpdateGrid(viewModel.Groups[0].Items);
-            //VerticalPictures.SetBinding(VerticalPictures.ItemTemplate, "Images");
+            //InitializeComponent();
+            //Images = new ObservableCollection<Image>();
+            //Images. Add(viewModel.Groups[0].Items[2].Image);
+            //Images.Add(new Image { Source = "spanie" });
+
+
+            //VerticalPictures.ItemsSource = Images;
 
 
         }
-        private void InitializeTabs()
-        {
-            foreach (var group in viewModel.Groups)
+           private void InitializeTabs()
             {
-                SwitchTab.Children.Add(new Label { Text = group.Title });
+                foreach (var group in viewModel.Groups)
+                {
+                    SwitchTab.Children.Add(new Label { Text = group.Title });
+                }
             }
-        }
-        private void SegmentedControl_ItemTapped(object sender, int groupIterator)
-        {
-            UpdateGrid(viewModel.Groups[groupIterator].Items);
-        }
-        public void UpdateGrid(List<Item> group)
-        {
-            CleanGirdLayout();
-            for (int i = 0; i < Column; i++)
-                GirdLayout.ColumnDefinitions.Add(new ColumnDefinition());
-
-            for (int i = 0; i < group.Count / Column; i++)
-                GirdLayout.RowDefinitions.Add(new RowDefinition());
-
-            for (int j = 0; j < group.Count / Column; j++)
+            private void SegmentedControl_ItemTapped(object sender, int groupIterator)
+            {
+                UpdateGrid(viewModel.Groups[groupIterator].Items);
+            }
+            public void UpdateGrid(List<Item> group)
+            {
+                CleanGirdLayout();
                 for (int i = 0; i < Column; i++)
-                    GirdLayout.Children.Add(GetImageWithProperParams(group, i + (j * Column)), i, j);
-        }
-        private void CleanGirdLayout()
-        {
-            GirdLayout.Children.Clear();
-            GirdLayout.ColumnDefinitions.Clear();
-            GirdLayout.RowDefinitions.Clear();
-        }
-        private Image GetImageWithProperParams(List<Item> group, int currentIndex)
-        {
-            var tapGestureRecognizer = new TapGestureRecognizer();
+                    GirdLayout.ColumnDefinitions.Add(new ColumnDefinition());
 
-            tapGestureRecognizer.Tapped += (s, e) => {
-                TextField.Text += group[currentIndex].Text + " ";
-                Images.Add(group[currentIndex].Image);
-            };
-            group[currentIndex].Image.GestureRecognizers.Clear();
-            group[currentIndex].Image.GestureRecognizers.Add(tapGestureRecognizer);
+                for (int i = 0; i < group.Count / Column; i++)
+                    GirdLayout.RowDefinitions.Add(new RowDefinition());
 
-            return group[currentIndex].Image;
-        }
+                for (int j = 0; j < group.Count / Column; j++)
+                    for (int i = 0; i < Column; i++)
+                        GirdLayout.Children.Add(GetImageWithProperParams(group, i + (j * Column)), i, j);
+            }
+            private void CleanGirdLayout()
+            {
+                GirdLayout.Children.Clear();
+                GirdLayout.ColumnDefinitions.Clear();
+                GirdLayout.RowDefinitions.Clear();
+            }
+            private Image GetImageWithProperParams(List<Item> group, int currentIndex)
+            {
+                var tapGestureRecognizer = new TapGestureRecognizer();
+
+                tapGestureRecognizer.Tapped += (s, e) => {
+                    TextField.Text += group[currentIndex].Text + " ";
+                    //Images.Add(group[currentIndex].Image);
+                };
+                group[currentIndex].Image.GestureRecognizers.Clear();
+                group[currentIndex].Image.GestureRecognizers.Add(tapGestureRecognizer);
+
+                return group[currentIndex].Image;
+            }
     }
 }
